a324d97255d18334931355593ef68f4c1f1734a4
Eduardo Aquino <edutraquino@gmail.com>
2022-05-20 19:04:56 -0300

    feat: add invalid param string

--- backend/src/application/errors/business-logic-error.ts
+++ backend/src/application/errors/business-logic-error.ts
@@ -8,6 +8,7 @@ class BusinessLogicError extends Error {
 const ErrorMessages = {
   PALINDROME_INICIAL_EH_MAIOR: "O valor inicial não pode ser maior que o final",
   VALOR_INVALIDO: "Valor inválido",
+  PARAMETRO_INVALIDO: "Parâmetro inválido",
 };
 
 export { BusinessLogicError, ErrorMessages };
