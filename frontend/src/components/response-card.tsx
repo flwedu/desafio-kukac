79f8e2357943ff70e2ffe1d6c0d059c70855f8da
Eduardo Aquino <edutraquino@gmail.com>
2022-05-21 19:54:55 -0300

    refact: remove h2 title from component

--- frontend/src/components/response-card.tsx
+++ frontend/src/components/response-card.tsx
@@ -1,7 +1,6 @@
 export default function ResponseCard({ results }: { results: string }) {
   return (
     <div className="response card">
-      <h2>Resposta</h2>
       <p>Esse é o resultado: </p>
       <textarea readOnly name="results" id="results" value={results}></textarea>
     </div>
