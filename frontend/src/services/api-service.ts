2e502ad3c3584129caa68edc00cd7777de0d3168
Eduardo Aquino <edutraquino@gmail.com>
2022-05-21 13:40:27 -0300

    feat: add cors to api-service

--- frontend/src/services/api-service.ts
+++ frontend/src/services/api-service.ts
@@ -5,6 +5,7 @@ export class ApiService {
     try {
       const results = await fetch(this.url + path, {
         method: "GET",
+        mode: "cors",
         headers: {
           "content-type": "application/json",
           accept: "application/json",
@@ -24,6 +25,7 @@ export class ApiService {
       const results = await fetch(this.url + path, {
         body: query,
         method: "POST",
+        mode: "cors",
         headers: {
           "content-type": "application/json",
           accept: "application/json",
