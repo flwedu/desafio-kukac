59cf5464fe46d08418b27ffca34bf1951fd4fb5d
Eduardo Aquino <edutraquino@gmail.com>
2022-05-21 10:28:47 -0300

    feat: basic structure of desafio1 page

--- frontend/src/pages/desafio1.tsx
+++ frontend/src/pages/desafio1.tsx
@@ -1,7 +1,69 @@
+import { useState } from "react";
+import { PalindromeService } from "../services/palindrome-service";
+
+export type Desafio1Query = {
+  initial: string;
+  final: string;
+};
+
 export default function Desafio1() {
+  const service = new PalindromeService("http://localhost:3002/palindrome");
+
+  const [query, setQuery] = useState<Desafio1Query>({ initial: "", final: "" });
+  const [results, setResults] = useState("");
+
+  function handleChange(event: any) {
+    const { name, value } = event.target;
+    setQuery({ ...query, [name]: value });
+  }
+
+  function handleSubmit() {
+    if (query.initial.length > 0 && query.final.length > 0)
+      service.execute(query).then(setResults);
+  }
+
   return (
     <div>
       <h1>Desafio 1 - Palíndromos</h1>
+      <p>
+        Números palíndromos são aqueles que escritos da direita para esquerda ou
+        da esquerda para direita tem o mesmo valor. Exemplo: 929, 44, 97379.
+        <br />
+        Esse programa exibe todos os números palíndromos dentro de um intervalo
+        definido por você.
+      </p>
+      <div>
+        <input
+          type="number"
+          name="initial"
+          placeholder="Valor inicial"
+          value={query.initial}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="final"
+          placeholder="Valor final"
+          value={query.final}
+          onChange={handleChange}
+        />
+      </div>
+      <button name="submit" onClick={handleSubmit}>
+        Consultar
+      </button>
+
+      {results && (
+        <div>
+          <h2>Respostas</h2>
+          <p>Esse é o resultado: </p>
+          <textarea
+            readOnly
+            name="results"
+            id="results"
+            value={results}
+          ></textarea>
+        </div>
+      )}
     </div>
   );
 }
