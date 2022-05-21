3e6cb85c3b2aabffc3873b50bebe62075514fdb6
Eduardo Aquino <edutraquino@gmail.com>
2022-05-21 09:13:16 -0300

    refact: passing vehicles repo in config routes fn

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import configureRoutes from "./input/routes";
 import { configureExpressApp } from "./main/express-server";
+import { JsonVehicleRepository } from "./output/repositories/json-vehicle-repository";
 
+const vehiclesRepo = new JsonVehicleRepository("http://localhost:3001");
 const PORT = 3002;
-const routes = configureRoutes();
+const routes = configureRoutes(vehiclesRepo);
 const { listen } = configureExpressApp(routes);
 
 listen(PORT);
