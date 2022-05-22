a4707760841c4437b5ef468314fec53a0036b31b
Eduardo Aquino <edutraquino@gmail.com>
2022-05-22 11:03:03 -0300

    feat: parsing error message

--- backend/src/input/controllers/error-handler.ts
+++ backend/src/input/controllers/error-handler.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from "express";
 
-export function errorHandler(err: any, req: Request, res: Response) {
+export function errorHandler(err: Error, req: Request, res: Response) {
   if (err.name === "BusinessLogicError") {
-    return res.status(400).json(err.message);
+    return res.status(400).json(JSON.stringify(err.message));
   }
-  return res.status(500).json(err.message);
+  return res.status(500).json(JSON.stringify(err.message));
 }
