5d78a3667791b6356e197dd42de8f555561284b6
Eduardo Aquino <edutraquino@gmail.com>
2022-05-22 14:36:42 -0300

    feat: add styles to /home and logo

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <nav>
+      <Link to="/">Início</Link>
       <Link to="/desafio1">Desafio 1</Link>
       <Link to="/desafio2">Desafio 2</Link>
       <Link to="/desafio3">Desafio 3</Link>
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,11 +1,20 @@
+import logo from "../../assets/logo.jpeg";
+
 export default function Home() {
   return (
-    <div>
+    <div className="card rounded shadow">
       <h1>App de Desafios Kukac</h1>
+
+      <img style={{ width: "10rem" }} src={logo} alt="Kukac logo" />
+
+      <hr />
       <p>Criado por Eduardo Aquino</p>
 
       <p>
-        Para saber mais sobre mim: <a href="https://flwedu.github.io/"></a>
+        Para saber mais sobre mim:{" "}
+        <a target={"_blank"} href="https://flwedu.github.io/">
+          https://flwedu.github.io/
+        </a>
       </p>
     </div>
   );
