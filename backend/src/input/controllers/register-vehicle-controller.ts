342166d64dd11cb908410dd88c874c764dc3ed2a
Eduardo Aquino <edutraquino@gmail.com>
2022-05-22 11:56:51 -0300

    refact: props = any

--- backend/src/input/controllers/register-vehicle-controller.ts
+++ backend/src/input/controllers/register-vehicle-controller.ts
@@ -1,6 +1,4 @@
 import { NextFunction, Request, Response } from "express";
-import { CarProps } from "../../application/domain/vehicle/car";
-import { MotorcycleProps } from "../../application/domain/vehicle/motorcycle";
 import RegisterCarUseCase from "../../application/usecases/register-car";
 import RegisterMotorcycleUseCase from "../../application/usecases/register-motorcycle";
 import IVehicleRepository from "../../output/repositories/vehicle-repository";
@@ -11,14 +9,13 @@ class RegisterVehicleController {
   async handle(req: Request, res: Response, next: NextFunction) {
     try {
       const type = req.body.type;
+      const props = req.body;
       let results;
 
       if (type === "car") {
-        const props: CarProps = req.body;
         results = await new RegisterCarUseCase(this.repository).execute(props);
       }
       if (type === "motorcycle") {
-        const props: MotorcycleProps = req.body;
         results = await new RegisterMotorcycleUseCase(this.repository).execute(
           props
         );
