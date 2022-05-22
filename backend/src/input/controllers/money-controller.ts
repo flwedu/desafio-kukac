4e2245185b17dafd429a6a22e8faeb256bbfcd92
Eduardo Aquino <edutraquino@gmail.com>
2022-05-22 09:27:06 -0300

    feat: add status(200) for success

--- backend/src/input/controllers/money-controller.ts
+++ backend/src/input/controllers/money-controller.ts
@@ -6,7 +6,7 @@ export class MoneyController {
     try {
       const { money, price } = req.body;
       const results = new FindMinimumAmountOfNotes().execute(money, price);
-      return res.json(results);
+      return res.status(200).json(results);
     } catch (error) {
       next(error);
     }
